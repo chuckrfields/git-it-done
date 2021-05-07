f5a24741779ef503f64999d56f16c1b86d4660db
Chuck Fields <info@spacetechcorp.com>
2021-05-07 14:14:50 -0400

    getUserRepos now accepts username parameter

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-}
+var getUserRepos = function(user) {
+    // format the github url
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+    fetch(apiURL).then(function(response) {
+        // The json() method returns another Promise, 
+        // hence the extra then() method, whose callback function captures the actual data.
+        response.json().then(function(data) {
+            console.log(data);
+        })
+      });
+};
 
 getUserRepos();
